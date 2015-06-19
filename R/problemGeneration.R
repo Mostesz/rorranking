1553ed3126c59f226467996e6b21f5752be878b6
Mostesz <jakub.wasikowski@gmail.com>
2015-06-19 08:15:35 +0200

    Add duplications removing during performances generation and add preferences generation

--- R/problemGeneration.R
+++ R/problemGeneration.R
@@ -16,12 +16,58 @@ generatePerformances <- function(crits.nr, alts.nr, min.value = 0, max.value = 1
         }
       }
     }
-    if (!has.domination) {
+    has.duplications = FALSE
+    for(i in 1:crits.nr) {
+      if (length(unique(perfs[,i])) != alts.nr) {
+        has.duplications = TRUE
+        break
+      }
+    }
+    if (!has.domination && !has.duplications) {
       break
     }
   }
   
   return(perfs)
+}
+
+isDominated <- function(perfs, dominant.idx, dominated.idx) {
+  for(i in 1:ncol(perfs)) {
+    if (perfs[dominated.idx, i] > perfs[dominant.idx, i]) {
+      return(FALSE)
+    }
+  }
+  return(TRUE)
+}
+
+getTriangularMatrixRowIndex <- function(value.idx, matrix.size) {
+  i <- (matrix.size-1) * (matrix.size) / 2 - value.idx
+  K <- floor((sqrt(8 * i+1)-1)/2)
+  return(matrix.size - 1 - K)
+}
+
+getTriangularMatrixColumnIndexUsingRowIndex <- function(value.idx, matrix.size, row.idx) {
+  return (value.idx - (matrix.size - 1) * (row.idx - 1) + (row.idx - 1) * row.idx / 2 + 1);
+}
+
+getTriangularMatrixColumnIndex <- function(value.idx, matrix.size) {
+  row <- getTriangularMatrixRowIndex(value.idx, matrix.size)
+  return(getTriangularMatrixColumnIndexUsingRowIndex(value.idx, matrix.size, row))
+}
+
+generatePreferences <- function(perfs, preferences.nr) {
+  result <- matrix(0, ncol=2, nrow=preferences.nr)
+  alts.nr <- nrow(perfs)  
+  edges.max <- alts.nr * (alts.nr - 1) / 2
+  triangular.mat.indexes <- sample(edges.max, preferences.nr)
+  alts.indexes <- sample(alts.nr)
+  for (pref.idx in 1:preferences.nr) {
+    row.idx <- getTriangularMatrixRowIndex(triangular.mat.indexes[pref.idx], alts.nr)
+    col.idx <- getTriangularMatrixColumnIndex(triangular.mat.indexes[pref.idx], alts.nr)
+    result[pref.idx, 1] <- alts.indexes[row.idx]
+    result[pref.idx, 2] <- alts.indexes[col.idx]
+  }
+  return(result)
 }
 
 generatePreferences <- function(perfs, preferences.nr) {
@@ -32,13 +78,4 @@ generatePreferences <- function(perfs, preferences.nr) {
 generateRankingPreferences <- function(perfs) {
   result <- t(combn(sample(nrow(perfs)), 2))
   return(result)
-}
-
-isDominated <- function(perfs, dominant.idx, dominated.idx) {
-  for(i in 1:ncol(perfs)) {
-    if (perfs[dominated.idx, i] > perfs[dominant.idx, i]) {
-      return(FALSE)
-    }
-  }
-  return(TRUE)
 }
